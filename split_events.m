c9b5b70cc8b05d8f4897bf44e23e3d0f55a41896
pskeba1 <pskeba1@jhu.edu>
2016-11-09 15:50:37 -0500

    started GUI event types

--- split_events.m
+++ split_events.m
@@ -11,6 +11,12 @@ function output = split_events(events_file)
 
 event_types = readtable('event_types.csv');
 
+x = inputdlg({'LM','Arousal Events','Apnea Events'},...
+              'Enter Event Names:', [1 50; 1 50; 1 50]); 
+
+
+
+
 params = getInput2(1);
 
 fid = fopen(events_file);
@@ -129,25 +135,27 @@ function generate_report(plm_outputs, params)
 %
 % TOD0: report log IMI, allow output to file
 
+fid = fopen('testfile.txt');
+
 ep = plm_outputs.epochstage;
 TST = sum(ep > 0,1)/120; TWT = sum(ep == 0,1)/120; 
 
-display(sprintf('Total sleep time: %.2f hours',TST));
+fprintf('Total sleep time: %.2f hours\n',TST);
 
 PLMSi = size(plm_outputs.PLMS,1)/TST;
-display(sprintf('PLMS index: %.2f per hour',PLMSi));
+fprintf('PLMS index: %.2f per hour\n',PLMSi);
 
 PLMWi = size(setdiff(plm_outputs.PLM,plm_outputs.PLMS,'rows'),1)/TWT;
-display(sprintf('PLMW index: %.2f per hour', PLMWi));
+fprintf('PLMW index: %.2f per hour\n', PLMWi);
 
 PLMS_Ni = sum(plm_outputs.PLMS(:,6) < 5)/(sum(ep > 0 & ep < 5)/120);
-display(sprintf('PLMS-N index: %.2f per hour',PLMS_Ni));
+fprintf('PLMS-N index: %.2f per hour\n',PLMS_Ni);
 
 PLMS_Ri = sum(plm_outputs.PLMS(:,6) == 5)/(sum(ep == 5)/120);
-display(sprintf('PLMS-R index: %.2f per hour',PLMS_Ri));
+fprintf('PLMS-R index: %.2f per hour\n',PLMS_Ri);
 
 PLMS_ai = sum(plm_outputs.PLMS(:,12) > 0)/TST;
-display(sprintf('PLMS-arousal index: %.2f per hour',PLMS_ai));
+fprintf('PLMS-arousal index: %.2f per hour\n',PLMS_ai);
 
 % Here we display PLMS/hr excluding CLM associated with apnea events. This
 % requires a reevaluation of periodicity, but I am unsure whether
@@ -158,90 +166,76 @@ display(sprintf('PLMS-arousal index: %.2f per hour',PLMS_ai));
 % The next 3 displays are indices for CLM associated with apnea events
 % (suppose I should say respiratory, since they're abbreviated rCLM)
 rCLMSi = sum(plm_outputs.CLMS(:,11) > 0)/TST;
-display(sprintf('rCLMS index: %.2f per hour',rCLMSi));
+fprintf('rCLMS index: %.2f per hour\n',rCLMSi);
 
 rCLMS_Ni = sum(plm_outputs.CLMS(:,11) > 0 & plm_outputs.CLMS(:,6) < 5)/...
     (sum(ep > 0 & ep < 5)/120);
-display(sprintf('rCLMS-N index: %.2f per hour',rCLMS_Ni));
+fprintf('rCLMS-N index: %.2f per hour\n',rCLMS_Ni);
 
 rCLMS_Ri = sum(plm_outputs.CLMS(:,11) > 0 & plm_outputs.CLMS(:,6) == 5)/...
     (sum(ep == 5)/120);
-display(sprintf('rCLMS-R index: %.2f per hour',rCLMS_Ri));
+fprintf('rCLMS-R index: %.2f per hour\n',rCLMS_Ri);
 
 % The next 2 displays are indices for CLM with IMI less than the min IMI
 short_CLMSi = sum(plm_outputs.CLMS(:,4) < params.minIMI)/TST;
-display(sprintf('short IMI CLMS index: %.2f per hour',short_CLMSi));
+fprintf('short IMI CLMS index: %.2f per hour\n',short_CLMSi);
 
 short_CLMWi = sum(plm_outputs.CLM(:,4) < params.minIMI & ...
     plm_outputs.CLM(:,6) == 0)/TWT;
-display(sprintf('short IMI CLMW index: %.2f per hour',short_CLMWi));
+fprintf('short IMI CLMW index: %.2f per hour\n',short_CLMWi);
 
 % Next 2 dipslays are are nonperiodic CLM
 np_CLMSi = sum(plm_outputs.CLMS(:,5) == 0)/TST;
-display(sprintf('nonperiodic CLMS index: %.2f per hour',np_CLMSi))
+fprintf('nonperiodic CLMS index: %.2f per hour\n',np_CLMSi);
 
 np_CLMWi = sum(plm_outputs.CLM(:,5) == 0 & plm_outputs.CLM(:,6) == 0)/TWT;
-display(sprintf('nonperiodic CLMW index: %.2f per hour',np_CLMWi))
+fprintf('nonperiodic CLMW index: %.2f per hour\n',np_CLMWi);
 
 % Next 4 are some duration stuff
 PLMS_dur = mean(plm_outputs.PLMS(:,3));
-display(sprintf('mean PLMS duration: %.2f s',PLMS_dur));
+fprintf('mean PLMS duration: %.2f s\n',PLMS_dur);
 
 PLMS_Ndur = mean(plm_outputs.PLMS(plm_outputs.PLMS(:,6) < 5,3));
-display(sprintf('mean PLMS-N duration: %.2f s',PLMS_Ndur));
+fprintf('mean PLMS-N duration: %.2f s\n',PLMS_Ndur);
 
 PLMS_Rdur = mean(plm_outputs.PLMS(plm_outputs.PLMS(:,6) == 5,3));
-display(sprintf('mean PLMS-R duration: %.2f s',PLMS_Rdur));
+fprintf('mean PLMS-R duration: %.2f s\n',PLMS_Rdur);
 
 PLMW_dur = mean(plm_outputs.PLM(plm_outputs.PLM(:,6) == 0,3));
-display(sprintf('mean PLMW-N duration: %.2f s',PLMW_dur));
+fprintf('mean PLMW-N duration: %.2f s\n',PLMW_dur);
 
 % Next 4 are some IMI stuff
 PLMS_imi = mean(plm_outputs.PLMS(plm_outputs.PLMS(:,9) == 0,4));
-display(sprintf('mean PLMS IMI: %.2f s',PLMS_imi));
+fprintf('mean PLMS IMI: %.2f s\n',PLMS_imi);
 
 PLMS_Nimi = mean(plm_outputs.PLMS(plm_outputs.PLMS(plm_outputs.PLMS(:,9) == 0,6) < 5,4));
-display(sprintf('mean PLMS-N IMI: %.2f s',PLMS_Nimi));
+fprintf('mean PLMS-N IMI: %.2f s\n',PLMS_Nimi);
 
 PLMS_Rimi = mean(plm_outputs.PLMS(plm_outputs.PLMS(plm_outputs.PLMS(:,9) == 0,6) == 5,4));
-display(sprintf('mean PLMS-R IMI: %.2f s',PLMS_Rimi));
+fprintf('mean PLMS-R IMI: %.2f s\n',PLMS_Rimi);
 
 PLMW_imi = mean(plm_outputs.PLM(plm_outputs.PLM(plm_outputs.PLMS(:,9) == 0,6) == 0,4));
-display(sprintf('mean PLMW-N IMI: %.2f s',PLMW_imi));
+fprintf('mean PLMW-N IMI: %.2f s\n',PLMW_imi);
 
 % The next 2 displays are duration for CLM with IMI less than the min IMI
 short_CLMSdur = mean(plm_outputs.CLMS(plm_outputs.CLMS(:,4) < params.minIMI,3));
-display(sprintf('short IMI CLMS duratoin: %.2f s',short_CLMSdur));
+fprintf('short IMI CLMS duratoin: %.2f s\n',short_CLMSdur);
 
 short_CLMWdur = mean(plm_outputs.CLM(plm_outputs.CLM(:,4) < params.minIMI & ...
     plm_outputs.CLM(:,6) == 0,3));
-display(sprintf('short IMI CLMW duration: %.2f s',short_CLMWdur));
+fprintf('short IMI CLMW duration: %.2f s\n',short_CLMWdur);
 
 right_mPLMSi = sum(plm_outputs.PLMS(:,13) == 1)/TST;
-display(sprintf('right monolateral PLMS index: %.2f per hour',right_mPLMSi));
+fprintf('right monolateral PLMS index: %.2f per hour\n',right_mPLMSi);
 
 left_mPLMSi = sum(plm_outputs.PLMS(:,13) == 2)/TST;
-display(sprintf('left monolateral PLMS index: %.2f per hour',left_mPLMSi));
+fprintf('left monolateral PLMS index: %.2f per hour\n',left_mPLMSi);
 
 bPLMSi = sum(plm_outputs.PLMS(:,13) == 3)/TST;
-display(sprintf('bilateral PLMS index: %.2f per hour',bPLMSi));
+fprintf('bilateral PLMS index: %.2f per hour\n',bPLMSi);
 end
 
 function [in,cancel] = getInput2(ask)
-%% [in] = getInput2(fs, ask)
-% Display a dialog window that asks the user for several initial conditions
-% and parameters. Store these conditions in the 'in' structure, to be
-% passed around the main function. Program will terminate if cancel is
-% true. Based off of the file 'inputsdlg.m', which is a script by Takeshi 
-% Ikuma that expands the functionality of matlab's built-in input dialogs.
-%
-% See the bottom of this function for several parameters related to the
-% classification of PLM that most users will have no desire to change, but
-% can do so here.
-%
-% inputs:
-%   - fs - sampling rate (for filter estimates)
-%   - ask - if false, just returns the default struct
 
 if ~ask
     in = struct('ars',true,'aps',true,'maxdur',10,'bmaxdur',15,...
