150f4b561663d197fa3ac6155a162ba09c54b1a5
pskeba1 <pskeba1@jhu.edu>
2016-12-07 17:15:57 -0500

    reporting

--- generate_report.m
+++ generate_report.m
@@ -19,7 +19,7 @@ T3T = size(ep(ep == 3),1)/120;
 TREMT = size(ep(ep == 5),1)/120;
 TnREMT = size(ep(ep > 0 & ep < 5),1)/120;
 
-%% Indices during entire night
+% Indices during entire night
 col3 = cell(15,1);
 col3{1} = datestr(TRT/24,'HH:MM:SS');
 col3{1+1} = size(plm_results.CLM,1)/TRT;
@@ -38,7 +38,7 @@ col3{13+1} = size(apnea,1);
 col3{14+1} = size(plm_results.CLMr(plm_results.CLMr(:,11)>0),1);
 col3{15+1} = size(arousal,1)/TRT;
 
-%% Indices during sleep
+% Indices during sleep
 col4 = cell(15,1);
 col4{1} = datestr(TST/24,'HH:MM:SS');
 col4{1+1} = size(plm_results.CLMS,1)/TST;
@@ -62,7 +62,7 @@ col4{14+1} = size(plm_results.CLMr(plm_results.CLMr(:,11)>0 & ...
     plm_results.CLMr(:,6) > 0),1);
 col4{15+1} = size(arousal,1)/TST; % TODO  arousal in sleep stage
 
-%% Indices during wake
+% Indices during wake
 PLMW = plm_results.PLM(plm_results.PLM(:,6) == 0,:);
 CLMW = plm_results.CLM(plm_results.CLM(:,6) == 0,:);
 col5 = cell(15,1);
@@ -88,7 +88,7 @@ col5{14+1} = size(plm_results.CLMr(plm_results.CLMr(:,11)>0 & ...
     plm_results.CLMr(:,6) == 0),1);
 col5{15+1} = size(arousal,1)/TWT; % TODO  arousal in sleep stage
 
-%% Indices during stage 1
+% Indices during stage 1
 PLM1 = plm_results.PLM(plm_results.PLM(:,6) == 1,:);
 CLM1 = plm_results.CLM(plm_results.CLM(:,6) == 1,:);
 col6 = cell(15,1);
@@ -116,7 +116,7 @@ col6{15+1} = size(arousal,1)/T1T; % TODO  arousal in sleep stage
 
 outtable = table(col1,col2,col3,col4,col5,col6);
 
-%% Indices during stage 2
+% Indices during stage 2
 PLM2 = plm_results.PLM(plm_results.PLM(:,6) == 2,:);
 CLM2 = plm_results.CLM(plm_results.CLM(:,6) == 2,:);
 col7 = cell(15,1);
@@ -142,7 +142,7 @@ col7{14+1} = size(plm_results.CLMr(plm_results.CLMr(:,11)>0 & ...
     plm_results.CLMr(:,6) == 2),1);
 col7{15+1} = size(arousal,1)/T2T; % TODO  arousal in sleep stage
 
-%% Indices during stage 3
+% Indices during stage 3
 PLM3 = plm_results.PLM(plm_results.PLM(:,6) == 3,:);
 CLM3 = plm_results.CLM(plm_results.CLM(:,6) == 3,:);
 col8 = cell(15,1);
@@ -168,7 +168,7 @@ col8{14+1} = size(plm_results.CLMr(plm_results.CLMr(:,11)>0 & ...
     plm_results.CLMr(:,6) == 3),1);
 col8{15+1} = size(arousal,1)/T3T; % TODO  arousal in sleep stage
 
-%% Indices during REM
+% Indices during REM
 PLMREM = plm_results.PLM(plm_results.PLM(:,6) == 5,:);
 CLMREM = plm_results.CLM(plm_results.CLM(:,6) == 5,:);
 col9 = cell(15,1);
@@ -194,7 +194,7 @@ col9{14+1} = size(plm_results.CLMr(plm_results.CLMr(:,11)>0 & ...
     plm_results.CLMr(:,6) == 5),1);
 col9{15+1} = size(arousal,1)/TREMT; % TODO  arousal in sleep stage
 
-%% Indices during REM
+% Indices during REM
 PLMnREM = plm_results.PLM(plm_results.PLM(:,6) > 0 & ...
     plm_results.PLM(:,6) < 5,:);
 CLMnREM = plm_results.CLM(plm_results.CLM(:,6) > 0 & ...
@@ -231,10 +231,12 @@ col10{15+1} = size(arousal,1)/TnREMT; % TODO  arousal in sleep stage
 
 
 colnames = {'Event','Metric','TIB','TST','Wake','N1','N2','N3','REM','NREM'};
-outtable = table(col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,...
-    'VariableNames',colnames)
 
-%% Plotting procedure
+% PLM Results
+outtable = table(col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,...
+    'VariableNames',colnames);
+
+% Plotting procedure
 lin_labels = {'2s','10s','20s','30s','40s','50s','60s','70s','80s','90s'};
 log_labels = {'2s','10s','20s','40s','60s','90s'};
 
@@ -269,4 +271,7 @@ title('CLMS intermovement intervals, log scale')
 xlim([log(2),log(90)])
 set(gca,'xtick',[log(2),log(10),log(20),log(40),log(60),log(90)]);
 set(gca,'xticklabel',log_labels);
+
+%% Report Generation
+
 end
